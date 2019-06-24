4a3aa4f31e8dc5c5833c37ec87a00284720d7cfc
Nayan707 <51816880+Nayan707@users.noreply.github.com>
2019-06-24 10:07:32 +0530

    Add files via upload

--- ICP_Python.py
+++ ICP_Python.py
@@ -1,15 +1,20 @@
+
 import open3d as o3d
 import numpy as np
 import pptk
-
+import pandas as pd
+import rope
+E =[]
+P =[]
+Q=[]
 print("Testing IO for point cloud")
 pcd1 = o3d.io.read_point_cloud("bun045.ply")
 print(pcd1)
 data1 = np.asarray(pcd1.points)
 print(type(data1))
 print(np.asarray(pcd1.points).shape)
-v=pptk.viewer(data1)
-v.attributes(data1+1)
+#v=pptk.viewer(data1)                            
+#v.attributes(data1+1)
 # ==========9===================================================================
 # v=pptk.viewer(data1)
 # v.attributes(data1+1)
@@ -22,8 +27,8 @@ data2 = np.asarray(pcd2.points)
 print(type(data2))
 print(np.asarray(pcd2.points).shape)
 
-v=pptk.viewer(data2)
-v.attributes(data2+1)
+#v=pptk.viewer(data2)
+#v.attributes(data2+1)
 
 def Average(array): 
     return sum(array) / len(array) 
@@ -32,6 +37,56 @@ x_data1 = data1[:,0]
 y_data1 = data1[:,1]
 z_data1 = data1[:,2]
 
-x_avg = Average(x_data1)
-y_avg = Average(y_data1)
-z_avg = Average(z_data1)
+x_avg_data1 = Average(x_data1)
+y_avg_data1 = Average(y_data1)
+z_avg_data1 = Average(z_data1)
+mean_data1 = [x_avg_data1, y_avg_data1, z_avg_data1] 
+
+
+#=====================================
+
+x_data2 = data2[:,0]
+y_data2 = data2[:,1]
+z_data2 = data2[:,2]
+
+x_avg_data2 = Average(x_data2)
+y_avg_data2 = Average(y_data2)
+z_avg_data2 = Average(z_data2)
+
+mean_data2 = [x_avg_data2, y_avg_data2, z_avg_data2]
+
+data1_centroid = data1 - mean_data1
+data2_centroid = data2 - mean_data2
+#z_avg_data3 = Average(data1_centroid[:,2])
+#for i in range(0, len(x_data1)-1):
+A = [1,2,3]
+C = np.float64(A)
+
+B = [11,12,13]
+D = np.float64(B)
+dist = np.linalg.norm(data1_centroid[1,:]-data2_centroid[1,:])
+dist2 = np.linalg.norm(C-D)
+
+
+# =============================================================================
+# # =============================================================================
+# for k in range(0,len(x_data1)):
+#     for i in range(0,len(x_data1)):
+#        # ist2 = np.linalg.norm(C-D)
+#         ist2 = np.linalg.norm(data1_centroid[k,:]-data2_centroid[i,:])
+#         E.append(ist2)
+#     P.append(E.index(min(E))) 
+#     E=[]
+#  
+# =============================================================================
+
+    
+    
+    
+    
+    
+    
+    
+
+
+ 
