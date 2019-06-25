3303a041d8e5e160af60d356d674269a1387f50f
siddhantb <siddhant2697@gmail.com>
2019-06-25 10:44:11 +0530

    Add code for rest of the process (work remains for a complete training process)

--- ICP_python_sid.py
+++ ICP_python_sid.py
@@ -8,6 +8,8 @@ from tqdm import tqdm
 # import pptk
 import numpy as np
 
+max_iterations = 100
+thresh = 0.001
 
 if cfg.USER is True:
     static_cloud = str(input('Enter the path to the static point cloud: '))
@@ -28,8 +30,15 @@ else:
 
 static_numpy = loader.down_sampled_numpy(static_cloud)
 moving_numpy = loader.down_sampled_numpy(moving_cloud)
-# v1 = pptk.viewer(static_numpy)
-# v1.attributes(static_numpy + 100)
+
+latest_moving_numpy = moving_numpy
+
+if len(static_numpy) > len(moving_numpy):
+    static_numpy = static_numpy[:len(moving_numpy), :]
+elif len(static_numpy) < len(moving_numpy):
+    moving_numpy = moving_numpy[:len(static_numpy), :]
+else:
+    pass
 
 correspondence_dict = {}
 
@@ -40,48 +49,130 @@ for i in tqdm(range(len(moving_numpy))):
     else:
         correspondence_dict['Q'] = [point]
     if 'p' in correspondence_dict.keys():
-        correspondence_dict['p'].append(moving_numpy[i])
+        correspondence_dict['p'].append(static_numpy[i])
     else:
-        correspondence_dict['p'] = [moving_numpy[i]]
+        correspondence_dict['p'] = [static_numpy[i]]    # There might be some confusion here
 
-mu_y = helper.centroid(correspondence_dict['Q'])
-mu_p = helper.centroid(correspondence_dict['p'])
+for i in range(max_iterations):
+    for j in range(len(latest_moving_numpy)):
+        point, minimum = helper.find_min_distance_point(latest_moving_numpy[i], static_numpy)
+        pass
 
-y_i_dash = correspondence_dict['Q'] - mu_y
-p_i_dash = correspondence_dict['p'] - mu_p
+# mu_y = helper.centroid(correspondence_dict['Q'])
+# mu_p = helper.centroid(correspondence_dict['p'])
+#
+# y_i_dash = correspondence_dict['Q'] - mu_y
+# p_i_dash = correspondence_dict['p'] - mu_p
+#
+# S_xx = helper.dot_product(p_i_dash, y_i_dash, 0, 0)
+# S_xy = helper.dot_product(p_i_dash, y_i_dash, 0, 1)
+# S_xz = helper.dot_product(p_i_dash, y_i_dash, 0, 2)
+# S_yx = helper.dot_product(p_i_dash, y_i_dash, 1, 0)
+# S_yy = helper.dot_product(p_i_dash, y_i_dash, 1, 1)
+# S_yz = helper.dot_product(p_i_dash, y_i_dash, 1, 2)
+# S_zx = helper.dot_product(p_i_dash, y_i_dash, 2, 0)
+# S_zy = helper.dot_product(p_i_dash, y_i_dash, 2, 1)
+# S_zz = helper.dot_product(p_i_dash, y_i_dash, 2, 2)
+#
+# N = np.array([[S_xx + S_yy + S_zz, S_yz - S_zy, - S_xz + S_zx, S_xy - S_yz],
+#               [S_yz - S_zy, S_xx - S_zz - S_yy, S_xy + S_yx, S_xz + S_zx],
+#               [-S_xz + S_zx, S_xy + S_yx, S_yy - S_zz - S_xx, S_yz + S_zy],
+#               [S_xy - S_yz, S_xz + S_zx, S_yz + S_zy, S_zz - S_yy - S_xx]])
+#
+# w, v = np.linalg.eig(N)
+# q = v[:, 0]
+#
+# q0, q1, q2, q3 = q[0], q[1], q[2], q[3]
+#
+# Q_bar = np.array([[q0, -q1, -q2, -q3],
+#                   [q1, q0, q3, -q2],
+#                   [q2, -q3, q0, q1],
+#                   [q3, q2, -q1, q0]])
+#
+# Q = np.array([[q0, -q1, -q2, -q3],
+#               [q1, q0, -q3, q2],
+#               [q2, q3, q0, -q1],
+#               [q3, -q2, q1, q0]])
+#
+# R = np.matmul(np.transpose(Q_bar), Q)
+# R = R[1:, 1:]
+#
+# Sp, D = 0, 0
+#
+# for i in range(len(y_i_dash)):
+#     D += np.matmul(np.transpose(y_i_dash[i]), y_i_dash[i])
+#     Sp += np.matmul(np.transpose(p_i_dash[i]), p_i_dash[i])
+#
+# s = np.sqrt(D/Sp)
+# t = mu_y - s * np.matmul(R, mu_p)
+#
+# err = 0
+# for i in range(len(y_i_dash)):
+#     d = static_numpy[i] - (s * np.matmul(R, moving_numpy[i]) + t)
+#     err += np.matmul(np.transpose(d), d)
+#
+# print('Error in first epoch is: {}'.format(err))
 
 
-S_xx = helper.dot_product(p_i_dash, y_i_dash, 0, 0)
-S_xy = helper.dot_product(p_i_dash, y_i_dash, 0, 1)
-S_xz = helper.dot_product(p_i_dash, y_i_dash, 0, 2)
-S_yx = helper.dot_product(p_i_dash, y_i_dash, 1, 0)
-S_yy = helper.dot_product(p_i_dash, y_i_dash, 1, 1)
-S_yz = helper.dot_product(p_i_dash, y_i_dash, 1, 2)
-S_zx = helper.dot_product(p_i_dash, y_i_dash, 2, 0)
-S_zy = helper.dot_product(p_i_dash, y_i_dash, 2, 1)
-S_zz = helper.dot_product(p_i_dash, y_i_dash, 2, 2)
+def find_alignment(static_numpy, latest_moving_numpy):
+    if len(static_numpy) > len(latest_moving_numpy):
+        static_numpy = static_numpy[:len(latest_moving_numpy), :]
+    elif len(static_numpy) < len(latest_moving_numpy):
+        latest_moving_numpy = latest_moving_numpy[:len(static_numpy), :]
+    else:
+        pass
 
-N = np.array([[S_xx + S_yy + S_zz, S_yz - S_zy, - S_xz + S_zx, S_xy - S_yz],
-              [S_yz - S_zy, S_xx - S_zz - S_yy, S_xy + S_yx, S_xz + S_zx],
-              [-S_xz + S_zx, S_xy + S_yx, S_yy - S_zz - S_xx, S_yz + S_zy],
-              [S_xy - S_yz, S_xz + S_zx, S_yz + S_zy, S_zz - S_yy - S_xx]])
+    mu_y = helper.centroid(correspondence_dict['Q'])
+    mu_p = helper.centroid(correspondence_dict['p'])
 
-w, v = np.linalg.eig(N)
-q = v[:, 0]
+    y_i_dash = correspondence_dict['Q'] - mu_y
+    p_i_dash = correspondence_dict['p'] - mu_p
 
-q0, q1, q2, q3 = q[0], q[1], q[2], q[3]
+    S_xx = helper.dot_product(p_i_dash, y_i_dash, 0, 0)
+    S_xy = helper.dot_product(p_i_dash, y_i_dash, 0, 1)
+    S_xz = helper.dot_product(p_i_dash, y_i_dash, 0, 2)
+    S_yx = helper.dot_product(p_i_dash, y_i_dash, 1, 0)
+    S_yy = helper.dot_product(p_i_dash, y_i_dash, 1, 1)
+    S_yz = helper.dot_product(p_i_dash, y_i_dash, 1, 2)
+    S_zx = helper.dot_product(p_i_dash, y_i_dash, 2, 0)
+    S_zy = helper.dot_product(p_i_dash, y_i_dash, 2, 1)
+    S_zz = helper.dot_product(p_i_dash, y_i_dash, 2, 2)
 
-Q_bar = np.array([[q0, -q1, -q2, -q3],
-                  [q1, q0, q3, -q2],
-                  [q2, -q3, q0, q1],
-                  [q3, q2, -q1, q0]])
+    N = np.array([[S_xx + S_yy + S_zz, S_yz - S_zy, - S_xz + S_zx, S_xy - S_yz],
+                  [S_yz - S_zy, S_xx - S_zz - S_yy, S_xy + S_yx, S_xz + S_zx],
+                  [-S_xz + S_zx, S_xy + S_yx, S_yy - S_zz - S_xx, S_yz + S_zy],
+                  [S_xy - S_yz, S_xz + S_zx, S_yz + S_zy, S_zz - S_yy - S_xx]])
 
-Q = np.array([[q0, -q1, -q2, -q3],
-              [q1, q0, -q3, q2],
-              [q2, q3, q0, -q1],
-              [q3, -q2, q1, q0]])
+    w, v = np.linalg.eig(N)
+    q = v[:, 0]
 
-R = np.matmul(np.transpose(Q_bar), Q)
-R = R[1:, 1:]
-print('Rotation matrix is: \n{}'.format(R))
+    q0, q1, q2, q3 = q[0], q[1], q[2], q[3]
 
+    Q_bar = np.array([[q0, -q1, -q2, -q3],
+                      [q1, q0, q3, -q2],
+                      [q2, -q3, q0, q1],
+                      [q3, q2, -q1, q0]])
+
+    Q = np.array([[q0, -q1, -q2, -q3],
+                  [q1, q0, -q3, q2],
+                  [q2, q3, q0, -q1],
+                  [q3, -q2, q1, q0]])
+
+    R = np.matmul(np.transpose(Q_bar), Q)
+    R = R[1:, 1:]
+
+    Sp, D = 0, 0
+
+    for i in range(len(y_i_dash)):
+        D += np.matmul(np.transpose(y_i_dash[i]), y_i_dash[i])
+        Sp += np.matmul(np.transpose(p_i_dash[i]), p_i_dash[i])
+
+    s = np.sqrt(D / Sp)
+    t = mu_y - s * np.matmul(R, mu_p)
+
+    err = 0
+    for i in range(len(y_i_dash)):
+        d = static_numpy[i] - (s * np.matmul(R, moving_numpy[i]) + t)
+        err += np.matmul(np.transpose(d), d)
+
+    return [s, R, t, err]
